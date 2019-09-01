64ce98b66c02c64436301ff768769e4bbc9750ff
Clément Cazaud <clement.cazaud@gmail.com>
2019-09-01 17:46:40 +0200

    Allows PHPUnit to mock final classes

---
+++ tests/PhpUnitHook/BypassFinalHook.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the opportus/object-mapper package.
+ *
+ * Copyright (c) 2018-2019 Clément Cazaud <clement.cazaud@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opportus\ObjectMapper\Tests\PhpUnitHook;
+
+use DG\BypassFinals;
+use PHPUnit\Runner\BeforeTestHook;
+
+/**
+ * The Bypass final hook.
+ *
+ * @package Opportus\ObjectMapper\Tests\Tests\PhpUnitHook
+ * @author  Clément Cazaud <opportus@gmail.com>
+ * @license https://github.com/opportus/object-mapper/blob/master/LICENSE MIT
+ */
+final class BypassFinalHook implements BeforeTestHook
+{
+    public function executeBeforeTest(string $test): void
+    {
+        BypassFinals::enable();
+    }
+}
+
