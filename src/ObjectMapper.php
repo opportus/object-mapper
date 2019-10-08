opportus/object-mapper

commit d0b6964934f26882beed439517bdca05d6c958cf

Improves ObjectMapper target instantiation

src/ObjectMapper.php
@@ -84,7 +84,7 @@ final class ObjectMapper implements ObjectMapperInterface
                 // Invokes target constructor...
                 $target = $targetClassReflection->newInstanceArgs($targetConstructorParameterPointValues);
             } else {
-                $target = new $target();
+                $target = new $context->getSourceClassFqn();
             }
         }
 

commit c89d904053346bc67bc0bc6d1cd5d8789d982bf3

Fixes bug introduced by d0b6964

src/ObjectMapper.php
@@ -84,7 +84,7 @@ final class ObjectMapper implements ObjectMapperInterface
                 // Invokes target constructor...
                 $target = $targetClassReflection->newInstanceArgs($targetConstructorParameterPointValues);
             } else {
-                $target = new $context->getSourceClassFqn();
+                $target = new $context->getTargetClassFqn();
             }
         }
 