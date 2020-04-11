1c4752abfb5e81658cc5224e356e228a3bb489c6
Clément Cazaud <clement.cazaud@gmail.com>
2020-04-11 12:19:23 +0200

    Updates copyright

--- src/Map/Strategy/PathFindingStrategy.php
+++ src/Map/Strategy/PathFindingStrategy.php
@@ -3,10 +3,10 @@
 /**
  * This file is part of the opportus/object-mapper package.
  *
- * Copyright (c) 2018-2019 Clément Cazaud <clement.cazaud@gmail.com>
+ * Copyright (c) 2018-2020 Clément Cazaud <clement.cazaud@gmail.com>
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  */
 
 namespace Opportus\ObjectMapper\Map\Strategy;
