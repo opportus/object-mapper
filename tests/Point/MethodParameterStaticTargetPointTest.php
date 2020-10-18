2ea5b83870011ad6c3fe93220298ca947646d5ed
Clément Cazaud <clement.cazaud@gmail.com>
2020-10-18 18:04:11 +0200

    Improves MethodParameterStaticTargetPointTest

--- tests/Point/MethodParameterStaticTargetPointTest.php
+++ tests/Point/MethodParameterStaticTargetPointTest.php
@@ -13,10 +13,15 @@ namespace Opportus\ObjectMapper\Tests\Point;
 
 use Opportus\ObjectMapper\Exception\InvalidArgumentException;
 use Opportus\ObjectMapper\Point\MethodParameterStaticTargetPoint;
+use Opportus\ObjectMapper\Point\ObjectPointInterface;
+use Opportus\ObjectMapper\Point\StaticTargetPointInterface;
+use Opportus\ObjectMapper\Point\TargetPointInterface;
+use Opportus\ObjectMapper\Tests\InvalidArgumentException as TestInvalidArgumentException;
+use Opportus\ObjectMapper\Tests\ObjectA;
 use PHPUnit\Framework\TestCase;
 
 /**
- * The method parameter static source point test.
+ * The method parameter static target point test.
  *
  * @package Opportus\ObjectMapper\Tests\Point
  * @author  Clément Cazaud <clement.cazaud@gmail.com>
@@ -24,251 +29,250 @@ use PHPUnit\Framework\TestCase;
  */
 class MethodParameterStaticTargetPointTest extends TestCase
 {
+    private const FQN_REGEX_PATTERN = '/^#?([A-Za-z0-9\\\_]+)::([A-Za-z0-9_]+)\(\)::\$([A-Za-z0-9_]+)$/';
+
     /**
-     * @dataProvider provideInvalidParameterPointFqns
-     * @param $invalidParameterPointFqn
-     * @throws InvalidArgumentException
+     * @dataProvider provideMethodParameterStaticTargetPointFqn
      */
-    public function testConstructException($invalidParameterPointFqn): void
+    public function testConstruct(string $fqn): void {
+        $point = new MethodParameterStaticTargetPoint($fqn);
+
+        static::assertInstanceOf(MethodParameterStaticTargetPoint::class, $point);
+        static::assertInstanceOf(StaticTargetPointInterface::class, $point);
+        static::assertInstanceOf(TargetPointInterface::class, $point);
+        static::assertInstanceOf(ObjectPointInterface::class, $point);
+    }
+
+    /**
+     * @dataProvider provideMethodParameterStaticTargetPointFqnException
+     */
+    public function testConstructException(string $fqn): void
     {
         $this->expectException(InvalidArgumentException::class);
-        new MethodParameterStaticTargetPoint($invalidParameterPointFqn);
+
+        new MethodParameterStaticTargetPoint($fqn);
     }
 
     /**
-     * @dataProvider provideParameterPointFqnTokens
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @throws InvalidArgumentException
+     * @dataProvider provideMethodParameterStaticTargetPointFqn
      */
-    public function testConstruct(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): void {
-        static::assertInstanceOf(
-            MethodParameterStaticTargetPoint::class,
-            new MethodParameterStaticTargetPoint(
-                \sprintf(
-                    '%s::%s()::$%s',
-                    $className,
-                    $methodName,
-                    $parameterName
-                )
-            )
-        );
-    }
+    public function testGetFqn(string $fqn): void
+    {
+        $point = new MethodParameterStaticTargetPoint($fqn);
 
-    /**
-     * @dataProvider provideParameterPointFqnTokens
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @throws InvalidArgumentException
-     */
-    public function testGetFqn(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): void {
-        $parameterPoint = $this->buildParameterPoint(
-            $className,
-            $methodName,
-            $parameterName
-        );
+        static::assertRegExp(self::FQN_REGEX_PATTERN, $point->getFqn());
 
         static::assertSame(
-            \sprintf('#%s::%s()::$%s', $className, $methodName, $parameterName),
-            $parameterPoint->getFqn()
+            \sprintf(
+                '#%s::%s()::$%s',
+                $this->getPointTargetFqn($fqn),
+                $this->getPointMethodName($fqn),
+                $this->getPointName($fqn)
+            ),
+            $point->getFqn()
         );
     }
 
     /**
-     * @dataProvider provideParameterPointFqnTokens
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @throws InvalidArgumentException
+     * @dataProvider provideMethodParameterStaticTargetPointFqn
      */
-    public function testGetClassFqn(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): void {
-        $parameterPoint = $this->buildParameterPoint(
-            $className,
-            $methodName,
-            $parameterName
-        );
+    public function testGetTargetFqn(string $fqn): void
+    {
+        $point = new MethodParameterStaticTargetPoint($fqn);
 
-        static::assertSame($className, $parameterPoint->getTargetFqn());
+        static::assertSame(
+            $point->getTargetFqn(),
+            $this->getPointTargetFqn($fqn)
+        );
     }
 
     /**
-     * @dataProvider provideParameterPointFqnTokens
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @throws InvalidArgumentException
+     * @dataProvider provideMethodParameterStaticTargetPointFqn
      */
-    public function testGetName(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): void {
-        $parameterPoint = $this->buildParameterPoint(
-            $className,
-            $methodName,
-            $parameterName
-        );
+    public function testGetName(string $fqn): void
+    {
+        $point = new MethodParameterStaticTargetPoint($fqn);
 
-        static::assertSame($parameterName, $parameterPoint->getName());
+        static::assertSame($point->getName(), $this->getPointName($fqn));
     }
 
     /**
-     * @dataProvider provideParameterPointFqnTokens
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @throws InvalidArgumentException
+     * @dataProvider provideMethodParameterStaticTargetPointFqn
      */
-    public function testGetMethodName(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): void {
-        $parameterPoint = $this->buildParameterPoint(
-            $className,
-            $methodName,
-            $parameterName
-        );
+    public function testGetMethodName(string $fqn): void
+    {
+        $point = new MethodParameterStaticTargetPoint($fqn);
 
-        static::assertSame($methodName, $parameterPoint->getMethodName());
+        static::assertSame(
+            $point->getMethodName(),
+            $this->getPointMethodName($fqn)
+        );
     }
 
-    /**
-     * @return array|string[][]
-     */
-    public function provideParameterPointFqnTokens(): array
+    public function provideMethodParameterStaticTargetPointFqn(): array
     {
         return [
             [
-                MethodParameterStaticTargetPointTestClass::class,
-                'privateMethod',
-                'privateMethodParameter',
+                \sprintf(
+                    '%s::%s()::$%s',
+                    ObjectA::class,
+                    'setA',
+                    'a'
+                ),
             ],
             [
-                MethodParameterStaticTargetPointTestClass::class,
-                'protectedMethod',
-                'protectedMethodParameter',
+                \sprintf(
+                    '#%s::%s()::$%s',
+                    ObjectA::class,
+                    'setA',
+                    'a'
+                ),
             ],
             [
-                MethodParameterStaticTargetPointTestClass::class,
-                'publicMethod',
-                'publicMethodParameter',
+                \sprintf(
+                    '%s::%s()::$%s',
+                    ObjectA::class,
+                    'setE',
+                    'e'
+                ),
+            ],
+            [
+                \sprintf(
+                    '#%s::%s()::$%s',
+                    ObjectA::class,
+                    'setE',
+                    'e'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::%s()::$%s',
+                    ObjectA::class,
+                    'setF',
+                    'f'
+                ),
+            ],
+            [
+                \sprintf(
+                    '#%s::%s()::$%s',
+                    ObjectA::class,
+                    'setF',
+                    'f'
+                ),
             ],
         ];
     }
 
-    /**
-     * @return array|array[]
-     */
-    public function provideInvalidParameterPointFqns(): array
+    public function provideMethodParameterStaticTargetPointFqnException(): array
     {
         return [
-            // Invalid syntax...
-            [\sprintf(
-                '%s::%s::$%s',
-                MethodParameterStaticTargetPointTestClass::class,
-                'publicMethod',
-                'publicMethodParameter'
-            )],
-            [\sprintf(
-                '%s::%s()::%s',
-                MethodParameterStaticTargetPointTestClass::class,
-                'publicMethod',
-                'publicMethodParameter'
-            )],
-            [\sprintf(
-                '%s::%s::%s',
-                MethodParameterStaticTargetPointTestClass::class,
-                'publicMethod',
-                'publicMethodParameter'
-            )],
-
-            // Invalid reflection...
-            [\sprintf(
-                '%s::%s()::$%s',
-                'InvalidClass',
-                'publicMethod',
-                'publicMethodParameter'
-            )],
-            [\sprintf(
-                '%s::%s()::$%s',
-                MethodParameterStaticTargetPointTestClass::class,
-                'invalidMethod',
-                'publicMethodParameter'
-            )],
-            [\sprintf(
-                '%s::%s()::$%s',
-                MethodParameterStaticTargetPointTestClass::class,
-                'publicMethod',
-                'invalidParameter'
-            )],
+            [
+                \sprintf(
+                    '~%s::%s()::$%s',
+                    ObjectA::class,
+                    'setA',
+                    'a'
+                ),
+            ],
+            [
+                \sprintf(
+                    '~%s::%s()::$%s',
+                    ObjectA::class,
+                    'setE',
+                    'e'
+                ),
+            ],
+            [
+                \sprintf(
+                    '~%s::%s()::$%s',
+                    ObjectA::class,
+                    'setF',
+                    'f'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::$%s',
+                    ObjectA::class,
+                    'f'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::%s()',
+                    ObjectA::class,
+                    'getA'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::%s()::$%s',
+                    'NonObject',
+                    'setA',
+                    'a'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::%s()::$%s',
+                    ObjectA::class,
+                    'nonMethod',
+                    'a'
+                ),
+            ],
+            [
+                \sprintf(
+                    '%s::%s()::$%s',
+                    ObjectA::class,
+                    'setA',
+                    'nonParameter'
+                ),
+            ],
         ];
     }
 
-    /**
-     * @param string $className
-     * @param string $methodName
-     * @param string $parameterName
-     * @return MethodParameterStaticTargetPoint
-     * @throws InvalidArgumentException
-     */
-    private function buildParameterPoint(
-        string $className,
-        string $methodName,
-        string $parameterName
-    ): MethodParameterStaticTargetPoint {
-        $parameterPointFqn = \sprintf(
-            '%s::%s()::$%s',
-            $className,
-            $methodName,
-            $parameterName
-        );
-
-        return new MethodParameterStaticTargetPoint($parameterPointFqn);
-    }
-}
-
-/**
- * The parameter object point test class.
- *
- * @package Opportus\ObjectMapper\Tests\Point
- * @author  Clément Cazaud <clement.cazaud@gmail.com>
- * @license https://github.com/opportus/object-mapper/blob/master/LICENSE MIT
- */
-class MethodParameterStaticTargetPointTestClass
-{
-    /**
-     * @param $privateMethodParameter
-     */
-    private function privateMethod($privateMethodParameter)
+    private function getPointTargetFqn(string $fqn): string
     {
+        if (false === \preg_match(self::FQN_REGEX_PATTERN, $fqn, $matches)) {
+            $message = \sprintf(
+                'The argument must match method parameter static target point FQN regex pattern %s, got %s.',
+                self::FQN_REGEX_PATTERN,
+                $fqn
+            );
+
+            throw new TestInvalidArgumentException(1, __METHOD__, $message);
+        }
+
+        return $matches[1];
     }
 
-    /**
-     * @param $protectedMethodParameter
-     */
-    protected function protectedMethod($protectedMethodParameter)
+    private function getPointMethodName(string $fqn): string
     {
+        if (false === \preg_match(self::FQN_REGEX_PATTERN, $fqn, $matches)) {
+            $message = \sprintf(
+                'The argument must match method parameter static target point FQN regex pattern %s, got %s.',
+                self::FQN_REGEX_PATTERN,
+                $fqn
+            );
+
+            throw new TestInvalidArgumentException(1, __METHOD__, $message);
+        }
+
+        return $matches[2];
     }
 
-    /**
-     * @param $publicMethodParameter
-     */
-    public function publicMethod($publicMethodParameter)
+    private function getPointName(string $fqn): string
     {
+        if (false === \preg_match(self::FQN_REGEX_PATTERN, $fqn, $matches)) {
+            $message = \sprintf(
+                'The argument must match method parameter static target point FQN regex pattern %s, got %s.',
+                self::FQN_REGEX_PATTERN,
+                $fqn
+            );
+
+            throw new TestInvalidArgumentException(1, __METHOD__, $message);
+        }
+
+        return $matches[3];
     }
 }
